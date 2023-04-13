13d79996e0b1f6a6892290800a046f78b2b831e1
unknown <darshansanghavi0505@gmail.com>
2023-04-13 12:47:27 +0530

    Home Page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top" data-bs-theme="dark">
          <div className="container-fluid">
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function Projects() {
   return (
-    <div className='container text-center my-5'>
-      I am Projects
-    </div>
+<div className="container text-center my-5">
+  <div className="row row-cols-3">
+    <div className="col">col-sm</div>
+    <div className="col">col-sm</div>
+    <div className="col">col-sm</div>
+    <div className="col">col-sm</div>
+    <div className="col">col-sm</div>
+    <div className="col">col-sm</div>
+    <div className="col">col-sm</div>
+  </div>
+</div>
   )
 }
